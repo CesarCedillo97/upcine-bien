43c871bb4a7f5192013d4cecd4dac1a74b8a7e31
JesusGuerrero97 <32663331+JesusGuerrero97@users.noreply.github.com>
2018-08-15 18:07:23 -0600

    zd

--- src/modelo/modeloProveedor.java
+++ src/modelo/modeloProveedor.java
@@ -5,10 +5,33 @@
  */
 package modelo;
 
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author Jesus
  */
 public class modeloProveedor extends modeloPrincipal{
     
+    public String[][] callObtenerDatos(){
+        //txtQuery devuelve TODOS los campos que se van a mostrar en la parte de datos
+        String txtQuery = "SELECT * FROM proveedor;";
+        return super.obtenerDatos(txtQuery);
+    }
+   
+    public DefaultTableModel callObtenerDatosTabla(){
+        //txtQueryTabla es la consulta que jalará los datos que irán en la tabla solamente
+        String txtQueryTabla = "SELECT Nombre_empresa, Nombre_responsable, Direccion, Telefono FROM proveedor;";
+        //Se obtienen los datos de la consulta de la tabla
+        String[][] datosTabla = super.obtenerDatos(txtQueryTabla);
+        //Se declaran los nombres de las columnas que llevará la table (Esta madre no tiene nada que ver con la base de datos si no con JTable)
+        String[] columnasTabla = new String[]{"Empresa","Responsable","Direccion","Telefono"};
+        return obtenerDatosTabla(datosTabla, columnasTabla);
+    }
+    
+    public DefaultTableModel callFiltrarTabla(String dato){
+        String[] columnas = {"Empresa","Responsable","Direccion","Telefono"};
+        String Query = "Nombre_empresa, Nombre_responsable, Direccion, Telefono FROM proveedor where Nombre_empresa LIKE '"+ dato +"%'";
+        return super.filtrarTabla(Query, columnas);
+    }
 }
